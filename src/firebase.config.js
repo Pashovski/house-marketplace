d372175f4e557ca70bb2609af7840c0342e2836a
Pasha <pashdoctor@gmail.com>
2022-01-25 15:30:10 -0500

    firebase config fixed

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-export const db = getFirestore
+export const db = getFirestore()
