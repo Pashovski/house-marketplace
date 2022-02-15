bdd42695764229d81e9685e713b986f576c0cbfd
Pasha <pashdoctor@gmail.com>
2022-02-15 17:00:44 -0500

    fix api key, and add geocoding

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -81,12 +81,24 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       )
 
-      const data = response.json()
+      const data = await response.json()
 
-      console.log(data)
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      geolocation.lnh = data.results[0]?.geometry.location.lng ?? 0
+
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0]?.formatted_address
+
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false)
+        toast.error('Please enter a correct address')
+        return
+      }
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
