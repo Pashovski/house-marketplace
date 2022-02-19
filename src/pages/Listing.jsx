514c8f43e6e7af44b1a969129cb5590096f9dfd5
pauldavidson <pashdoctor@gmail.com>
2022-02-19 18:33:47 -0500

    quick note and change to address

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,28 +7,28 @@ import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
 
 function Listing() {
-  const [listing, setListing] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  //   const [listing, setListing] = useState(null);
+  //   const [loading, setLoading] = useState(true);
+  //   const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
-  const navigate = useNavigate();
-  const params = useParams();
-  const auth = getAuth();
+  //   const navigate = useNavigate();
+  //   const params = useParams();
+  //   const auth = getAuth();
 
-  useEffect(() => {
-    const fetchListing = async () => {
-      const docRef = doc(db, "listings", params.listingId);
-      const docSnap = await getDoc(docRef);
+  //   useEffect(() => {
+  //     const fetchListing = async () => {
+  //       const docRef = doc(db, "listings", params.listingId);
+  //       const docSnap = await getDoc(docRef);
 
-      if (docSnap.exists()) {
-        console.log(docSnap.data());
-        setListing(docSnap.data());
-        setLoading(false);
-      }
-    };
+  //       if (docSnap.exists()) {
+  //         console.log(docSnap.data());
+  //         setListing(docSnap.data());
+  //         setLoading(false);
+  //       }
+  //     };
 
-    fetchListing();
-  }, [navigate, params.listingId]);
+  //     fetchListing();
+  //   }, [navigate, params.listingId]);
 
   return <div>LISTING</div>;
 }
