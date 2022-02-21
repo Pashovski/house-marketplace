8ced60d458bf5eeb5aced17c0e0d2eb161073573
Pasha <pashdoctor@gmail.com>
2022-02-21 12:28:34 -0500

    add contact component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+
+function Contact() {
+  const [message, setMessage] = useState('')
+  const [landlord, setLandlord] = useState(null)
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const params = useParams()
+
+  useEffect(() => {
+    const getLandlord = async () => {}
+  }, [params.landlord])
+  return <div>Contact</div>
+}
+
+export default Contact
